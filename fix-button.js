e900dc87a2b75c2b27c07422a7ae52ff4118e2fe
Vignesh0074 <vignexh.2003@gmail.com>
2026-08-28 21:55:01 +0530

    Updated project

--- fix-button.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'c:/Users/vv356/Downloads/Tiffin & Meal/customer-dashboard.html';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace('<button class="btn btn-primary mt-2">Select Meals</button>', '<button class="btn btn-primary mt-2" onclick="window.location.href=\'menu.html\'">Select Meals</button>');
-
-fs.writeFileSync(file, content);
-console.log('Fixed button');
